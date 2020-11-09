2bad468409f1d1e355c0bd814d3db322b523e893
jsmerki <jsm8577@gmail.com>
2020-11-08 20:14:31 -0700

    Re-adding fragment_courses

--- Scheduler/app/src/main/java/com/example/scheduler/CoursesFragment.java
+++ Scheduler/app/src/main/java/com/example/scheduler/CoursesFragment.java
@@ -1,5 +1,29 @@
 package com.example.scheduler;
 
-public class CoursesFragment {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-}
+import androidx.fragment.app.Fragment;
+
+    public class CoursesFragment extends Fragment {
+
+        Activity containerAcitivty;
+
+        public CoursesFragment(Activity container){
+            containerAcitivty = container;
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup fragContainer,
+                                 Bundle savedInstanceState) {
+
+            //Inflate layout to fit fragContainer View
+            View inflatedView = inflater.inflate(R.layout.fragment_courses, fragContainer, false);
+
+            return inflatedView;
+        }
+
+    }
